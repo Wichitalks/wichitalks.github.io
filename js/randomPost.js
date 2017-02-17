eb564e39ec40ddf5c2b013ae7ea6cba1892f83cd
Kevin Falting <kevinfalting@Kevins-MacBook-Pro.local>
2017-02-17 03:04:22 +0300

    fixed speaker section issue with it not displaying properly on different screen sizes. It now looks good at all screen widths.

--- js/randomPost.js
+++ js/randomPost.js
@@ -8,7 +8,7 @@ function generateRandomPosts()
 
         var randomIndexUsed = [];
         var counter = 0;
-        var numberOfPosts = 3;
+        var numberOfPosts = 4;
 
         var divRandomPosts = $("#random_posts");
 
@@ -23,8 +23,11 @@ function generateRandomPosts()
                 var postImage = posts[randomIndex].img;
                 var postSpeaker = posts[randomIndex].speaker;
 
-                divRandomPosts.append('<div class="col-lg-4 col-sm-6" style="padding: 5px;"><a href="' + postHREF + '" class="portfolio-box"><img src="' + postImage + '" class="img-responsive" alt=""><div class="portfolio-box-caption"><div class="portfolio-box-caption-content"><div class="project-category text-faded">' + postSpeaker + '<br>on</div><div class="project-name">' + postTitle + '</div></div></div></a></div>');
-
+                if (counter == 3) {
+                   divRandomPosts.append('<div class="col-lg-4 col-sm-6 hide_on_large_screen" style="padding: 5px;"><a href="' + postHREF + '" class="portfolio-box"><img src="' + postImage + '" class="img-responsive" alt=""><div class="portfolio-box-caption"><div class="portfolio-box-caption-content"><div class="project-category text-faded">' + postSpeaker + '<br>on</div><div class="project-name">' + postTitle + '</div></div></div></a></div>');
+                } else {
+                    divRandomPosts.append('<div class="col-lg-4 col-sm-6" style="padding: 5px;"><a href="' + postHREF + '" class="portfolio-box"><img src="' + postImage + '" class="img-responsive" alt=""><div class="portfolio-box-caption"><div class="portfolio-box-caption-content"><div class="project-category text-faded">' + postSpeaker + '<br>on</div><div class="project-name">' + postTitle + '</div></div></div></a></div>');
+                }
 
                 randomIndexUsed.push(randomIndex);
 
